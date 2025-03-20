772a91ce21122339844a4c6a1633441cdd88018f
plf-computing <plfcomputing@gmail.com>
2025-03-20 18:23:38 +0300

    `feat: updated home and discover components with new layout and content`

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "./hero/hero.component";
 import { DiscoverComponent } from "./discover/discover.component";
+import { WhyUsComponent } from "./why-us/why-us.component";
+import { GalleryComponent } from "./gallery/gallery.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, DiscoverComponent],
+  imports: [HeroComponent, DiscoverComponent, WhyUsComponent, GalleryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
